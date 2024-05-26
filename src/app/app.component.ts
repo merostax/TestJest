9f2bd1d95b76337291f33cb1c53221c61200c816
kt <k.themri@ostfalia.de>
2024-05-26 20:18:42 +0200

    first implementation _first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ButtonComponent} from "../models/button/button.component";
 
 @Component({
-  selector: 'app-root',
+  selector: 'git app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'jestTest';
 }
