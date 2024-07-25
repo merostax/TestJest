a8bc60f752ca95828877eef5d92c5463120ebab9
merostax <khalil.themri@gmail.com>
2024-07-25 20:22:45 +0200

    add image

--- src/chap1/<name of file tested>.spec.ts
+++ src/chap1/<name of file tested>.spec.ts
@@ -1,12 +1,27 @@
-describe('Chapter 2 tests', () => {
+describe('all chapters', () => {
+    describe('Chapter 1 tests', () => {
   it('Our first jest test', () => {
     expect(true).toBe(true);
   });
 
   it('Our second jest test', () => {
-    expect(false).toBe(false);
+    expect(2 + 3).not.toBe(4);
   });
   it('Our third jest test', () => {
-    expect(true&&false).toBe(false);
+      expect(2 + 2).toBe(4);
   });
 });
+
+describe('Chapter 2 tests', () => {
+    it('Our first jest test', () => {
+      expect(true).toBe(true);
+    });
+  
+    it('Our second jest test', () => {
+      expect(false).toBe(false);
+    });
+    it('Our third jest test', () => {
+      expect(true&&false).toBe(false);
+    });
+  });
+})
