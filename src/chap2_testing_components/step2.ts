2da84d5c5519319d98f1615d5e904cee7dcee745
merostax <khalil.themri@gmail.com>
2024-07-31 01:30:24 +0200

    add more info to step2

--- src/chap2_testing_components/step2.ts
+++ src/chap2_testing_components/step2.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { of } from 'rxjs';
 /*1*/
 describe('ContactEditComponent tests', () => {
   let fixture: ComponentFixture<ContactEditComponent>;
@@ -143,6 +144,83 @@ The detectChanges method triggers a change-detection cycle for the component; yo
 need to call it after initializing a component or changing a data-bound property value.
 After calling detectChanges, the updates to your component will be rendered in the
 DOM. */
+});
+          
+ 
+
+/*--------------------------------------------------------------------------------------------------------------*/
+
+/*4*/
+describe('ContactEditComponent tests', () => {
+  let fixture: ComponentFixture<ContactEditComponent>;
+  let component: ContactEditComponent;
+  let rootElement: DebugElement;
+  let contactService: ContactService;
+  let router: Router;
+  let route: ActivatedRoute;
+
+  beforeEach(async () => {
+    const contactServiceSpy = {
+      getContact: jest.fn(),
+      saveContact: jest.fn()
+    };
+    await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forRoot([]), 
+        ContactEditComponent
+      ],
+      providers: [
+        { provide: ContactService, useValue: contactServiceSpy },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { paramMap: { get: jest.fn().mockReturnValue('1') } }
+          }
+        }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContactEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    rootElement = fixture.debugElement;
+
+    contactService = TestBed.inject(ContactService);
+    router = TestBed.inject(Router);
+    route = TestBed.inject(ActivatedRoute);
+  });
+  describe('init tests', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();// note that to be truthfuly is not to be true,to be truthfuly is everything that is not null,not undefined...
+    });
+  });
+  
+  describe('load contact on init', () => {
+    it('should load existing contact on init', () => {
+      const contact = { id: 1, name: 'kt'}; // we create a contact that we see if ngoninit is working
+      jest.spyOn(contactService, 'getContact').mockReturnValue(of(contact));
+  // this is where the magic happens,
+  // spyon is a really powerful tool that u ll see often
+  // it takes 2 arguments first : object,second : method
+  // note that the contactservice here is the one mocked dont switch it up with the real one,
+  // however u wanna check the real one to know what to do with it,
+  // for example mockreturnvalue will return a value,
+// u see we used of--> which we use if we have an observable to return,
+// another thing to consider is there is that spyon has mockimplementation that u can mock the whole fucntion,
+// if we deal with promises, u wanna use mockResolvedValue(value)
+//Shorthand for:jest.fn().mockImplementation(() => Promise.resolve(value));
+// mockreturnValue(): jest.fn().mockImplementation(() => value);
+// see u can now create ur own mockimplementation 
+// check this link for more depth learning,https://jestjs.io/docs/mock-function-api
+      
+      component.ngOnInit();
+      expect(contactService.getContact).toHaveBeenCalledWith(1);
+      expect(component.contact).toEqual(contact);
+    });
 });
           
  
